888f4a43636de5e5437f90beb304ae0a7cda816c
Kunal Singh <kunaldsingh26@gmail.com>
2026-02-28 15:36:03 -0600

    Add iOS AR Xcode project

---
+++ ios/AR-CHESS/AR-CHESS/AR_CHESSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_CHESSApp.swift
+//  AR-CHESS
+//
+//  Created by Kunal Singh on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct AR_CHESSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
