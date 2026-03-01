2241d8de64b66acaf0002997740462f8605d7dcc
Kunal Singh <kunaldsingh26@gmail.com>
2026-02-28 19:20:58 -0600

    Port Fixes

--- server-app/main.py
+++ server-app/main.py
@@ -264,5 +264,5 @@ def get_game_moves(game_id: uuid.UUID) -> GameMoveListResponse:
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8000"))
+    port = int(os.getenv("PORT", "8080"))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
